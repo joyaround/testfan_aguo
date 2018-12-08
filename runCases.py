f31c3b90e530919412533b26d62cd5eb2ea5fcd6
aguo testfan <joyaround@126.com>
2018-12-08 17:05:12 +0800

    modified email function.

--- runCases.py
+++ runCases.py
@@ -18,8 +18,6 @@ mysuite.addTest(unittest.makeSuite(tryTestfan.Testfan))
 reportfilename = 'result2.html'
 fp = open(reportfilename, 'wb')
 runner =HTMLTestRunner.HTMLTestRunner(stream=fp,title='百度搜索测试报告',description='用例执行情况：')
-#执行测试套件
-# runner = unittest.TextTestRunner()
 runner.run(mysuite)
 
 # 设置smtplib所需的参数
@@ -39,33 +37,25 @@ msg['Subject'] = subject
 msg['From'] = 'Testfan_AGuo@163.com <testfan_aguo@163.com>'
 # 收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
 msg['To'] = ";".join(receiver)
-# msg['Date']='2012-3-16'
 
 # 构造文字内容
 text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.testfan.cn"
 text_plain = MIMEText(text, 'plain', 'utf-8')
 msg.attach(text_plain)
 
-# html = "test email"
-# text_html = MIMEText(html, 'html', 'utf-8')
-# text_html["Content-Disposition"] = 'attachment; filename="texthtml.html"'
-# msg.attach(text_html)
-
 # 构造附件
-sendfile = open(r'.\smail.py', 'rb').read()
+sendfile = open(reportfilename, 'rb').read()
 text_att = MIMEText(sendfile, 'base64', 'utf-8')
 text_att["Content-Type"] = 'application/octet-stream'
 # 附件可以重命名成aaa.txt，最好用原来文件名
-# text_att["Content-Disposition"] = 'attachment; filename="smail.py"'
+text_att["Content-Disposition"] = 'attachment; filename='+ reportfilename
 # 另一种实现方式
-text_att.add_header('Content-Disposition', 'attachment', filename=reportfilename)
+# text_att.add_header('Content-Disposition', 'attachment', filename=reportfilename)
 msg.attach(text_att)
 
 # 发送邮件
 smtp = smtplib.SMTP()
 smtp.connect('smtp.163.com')
-# 我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
-# smtp.set_debuglevel(1)
 smtp.login(username, password)
 smtp.sendmail(sender, receiver, msg.as_string())
 smtp.quit()
